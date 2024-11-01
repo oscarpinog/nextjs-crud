a293f37a81e4b8af943ff1ccb456e174f364e9f1
infodp <diprimiogustavo@gmail.com>
2024-11-01 17:41:03 -0300

    add methods crud

--- src/app/api/phones/[id]/route.tsx
+++ src/app/api/phones/[id]/route.tsx
@@ -2,13 +2,12 @@ import { NextResponse } from "next/server";
 
 const url = process.env.API_URL;
 
-// Traer un producto
 export async function GET(
   request: Request,
-  { params }: { params: Promise<{ id: string }> } // Define params como una Promesa
+  { params }: { params: Promise<{ id: string }> }
 ) {
-  const { id } = await params; // Espera a que params se resuelva
-  console.log('ID:', id); // Ahora puedes acceder a id
+  const { id } = await params;
+  console.log("ID:", id);
 
   try {
     const result = await fetch(`${url}/${id}`, {
@@ -27,9 +26,96 @@ export async function GET(
     const product = await result.json();
     return NextResponse.json({ data: product });
   } catch (error) {
-    console.error('Fetch Error:', error);
+    console.error("Fetch Error:", error);
     return NextResponse.json(
       { message: "Error fetching product" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  console.log("ID a eliminar:", id);
+
+  try {
+    const result = await fetch(`${url}/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "*/*",
+      },
+    });
+    const data = await result.json();    
+    if (result.status === 404) {
+      return NextResponse.json(
+        { message: "Product not found" },
+        { status: 404 }
+      );
+    }
+
+    if (result.status === 405) {
+      return NextResponse.json(
+        { message: "Method Not Allowed" },
+        { status: 405 }
+      );
+    }
+
+    return NextResponse.json({ message: data.message }, { status: 200 });
+  } catch (error) {
+    console.error("Error al eliminar:", error);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function PUT(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;  
+  try {    
+    const body = await request.json();    
+    const result = await fetch(`${url}/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "*/*",
+      },
+      body: JSON.stringify(body),
+    });
+    const data = result.ok ? await result.json() : null;    
+    if (result.status === 404) {
+      return NextResponse.json(
+        { message: "Product not found" },
+        { status: 404 }
+      );
+    }
+
+    if (result.status === 405) {
+      return NextResponse.json(
+        { message: "Method Not Allowed" },
+        { status: 405 }
+      );
+    }
+
+    if (!data) {
+      return NextResponse.json(
+        { message: "Empty response from API" },
+        { status: 204 }
+      );
+    }
+
+    return NextResponse.json(data, { status: 200 });
+  } catch (error) {
+    console.error("Error al actualizar:", error);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
       { status: 500 }
     );
   }
--- src/app/api/phones/route.tsx
+++ src/app/api/phones/route.tsx
@@ -4,16 +4,13 @@ const url = process.env.API_URL;
 
 export async function GET(request: Request) {
   try {
-    //Verificamos que la URL de la API esté definida para evitar errores por falta de configuración.
     if (!url) {
       return NextResponse.json(
         { error: "API URL not defined" },
         { status: 500 }
       );
-    }
-    //El uso de { cache: 'no-store' } garantiza que la petición siempre obtenga los datos más recientes.
+    }    
     const res = await fetch(url, { cache: "no-store" });
-
     if (!res.ok) {
       throw new Error(`Failed to fetch: ${res.statusText}`);
     }
@@ -30,18 +27,14 @@ export async function GET(request: Request) {
 }
 
 export async function POST(request: Request) {
-  try {
-    // Parseamos el cuerpo de la solicitud como JSON
+  try {    
     const body = await request.json();
-
     if (!url) {
       return NextResponse.json(
         { error: "API URL not defined" },
         { status: 500 }
       );
-    }
-
-    // Realizamos la petición POST a la API externa
+    }    
     const res = await fetch(url, {
       method: "POST",
       headers: {
